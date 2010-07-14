befd528083849a367d57b6ce60774650ec9174a7
Adam Podolnick <podman@gmail.com>
2010-07-13 21:38:24 -0400

    added mediainfo

---
+++ cookbooks/mediainfo/recipes/default.rb
@@ -0,0 +1,20 @@
+if ['solo', 'util'].include?(node[:instance_role])
+  mi_dir = "MediaInfo_CLI_GNU_FromSource"
+  mi_file = "MediaInfo_CLI_0.7.34_GNU_FromSource.tar.bz2"
+  mi_url = "http://downloads.sourceforge.net/project/mediainfo/binary/mediainfo/0.7.34/MediaInfo_CLI_0.7.34_GNU_FromSource.tar.bz2"
+  remote_file "/data/#{mi_file}" do
+    source "#{mi_url}"
+    owner node[:owner_name]
+    group node[:owner_name]
+    mode 0644
+    backup 0
+    not_if { FileTest.exists?("/data/#{mi_file}") }
+  end
+  execute "unarchive mi-to-install" do
+    command "cd /data && tar zxf #{mi_file} && sync"
+    not_if { FileTest.directory?("/data/#{mi_dir}") }
+  end
+  execute "build mi package" do
+    command "cd /data/#{mi_dir} && ./CLI_Compile.sh && cd /data/#{mi_dir}/MediaInfo/Project/GNU/CLI && make install"
+  end 
+end
