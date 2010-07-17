ec88edad3ac0dcca8e9e22b2f6226767ce6c98d0
Adam Podolnick <podman@gmail.com>
2010-07-17 16:01:44 -0400

    added monitrc for nodejs

--- cookbooks/nodejs/recipes/default.rb
+++ cookbooks/nodejs/recipes/default.rb
@@ -64,6 +64,24 @@ node[:applications].each do |app_name,data|
           not_if {FileTest.exists?("/data/#{app_name}/current/vendor/node/deps/lib/ceoip.node")}
         end
         
+        template "/etc/monit.d/nodejs.monitrc" do
+          source "nodejs.monit.erb"
+          owner "root"
+          group "root"
+          mode 0644
+          variables({
+            :app_name => app_name,
+            :user => node[:owner_name],
+            :worker_name => worker_name,
+            :framework_env => node[:environment][:framework_env]
+          })
+        end
+
+        bash "monit-reload-restart" do
+          user "root"
+          code "pkill -9 monit && monit"
+        end
+        
       end
     end
 end
