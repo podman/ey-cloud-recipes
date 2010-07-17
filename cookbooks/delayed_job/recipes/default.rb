ebb1f7ad6634c1c0040653d3c647dc36e5758c49
Adam Podolnick <podman@gmail.com>
2010-07-17 15:08:14 -0400

    trying engineyards launch script

--- cookbooks/delayed_job/recipes/default.rb
+++ cookbooks/delayed_job/recipes/default.rb
@@ -4,10 +4,11 @@ if ['solor', 'app', 'app_master'].include?(node[:instance_role])
 
   node[:applications].each do |app_name,data|
     
-    directory "/data/#{app_name}/shared/pids" do
-      owner node[:owner_name]
-      group node[:owner_name]
+    remote_file "/engineyard/bin/dj" do
+      owner "root"
+      group "root"
       mode 0755
+      source "dj"
     end
     
     template "/etc/monit.d/delayed_job_worker.#{app_name}.monitrc" do
