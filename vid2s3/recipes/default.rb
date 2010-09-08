2dd5b13ecfb5fccb808fab4fad4c038f8f428959
Adam Podolnick <podman@gmail.com>
2010-09-07 23:28:37 -0400

    added vid2s3 and make them only run on mongo instance

--- vid2s3/recipes/default.rb
+++ vid2s3/recipes/default.rb
@@ -1,14 +1,14 @@
 if node[:instance_role] == 'util' && node[:name].match(/^mongodb_/)
-  template "/etc/monit.d/rtmplite.monitrc" do
-    source "rtmplite.monitrc.erb"
+  template "/etc/monit.d/vid2s3.monitrc" do
+    source "vid2s3.monitrc.erb"
     owner node[:owner_name]
     group node[:owner_name]
     mode 0644
   end
   
-  execute "restart-monit-rtmplite" do
+  execute "restart-monit-vid2s3" do
     command "/usr/bin/monit reload && " +
-            "/usr/bin/monit restart rtmplite"
+            "/usr/bin/monit restart vid2s3"
     action :run
   end
 end
