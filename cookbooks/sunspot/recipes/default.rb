28d58afa72df025ef5dd9ef9a9f0caed3ae8edcb
Adam Podolnick <podman@gmail.com>
2011-06-04 15:45:07 -0400

    updated sunspot recipe to use solr 1.4.1

--- cookbooks/sunspot/recipes/default.rb
+++ cookbooks/sunspot/recipes/default.rb
@@ -48,10 +48,10 @@ node[:applications].each do |app,data|
     user node[:owner_name]
     group node[:owner_name]
     command("if [ ! -e /data/#{app}/jettyapps/solr ]; then cd /data/#{app}/jettyapps && " +
-            "wget -O apache-solr-1.4.0.tgz http://mirror.cc.columbia.edu/pub/software/apache/lucene/solr/1.4.0/apache-solr-1.4.0.tgz && " +
-            "tar -xzf apache-solr-1.4.0.tgz && " +
-            "mv apache-solr-1.4.0/example solr && " +
-            "rm -rf apache-solr-1.4.0; fi")
+            "wget -O apache-solr-1.4.1.tgz http://mirror.cc.columbia.edu/pub/software/apache/lucene/solr/1.4.1/apache-solr-1.4.1.tgz && " +
+            "tar -xzf apache-solr-1.4.1.tgz && " +
+            "mv apache-solr-1.4.1/example solr && " +
+            "rm -rf apache-solr-1.4.1; fi")
     action :run
   end
  
