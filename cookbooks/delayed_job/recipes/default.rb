b0a1fdfaf23f411a4b1a8c7a198c6853ffcac2d4
Adam Podolnick <podman@gmail.com>
2012-04-06 17:12:56 -0300

    more workers!

--- cookbooks/delayed_job/recipes/default.rb
+++ cookbooks/delayed_job/recipes/default.rb
@@ -11,17 +11,29 @@ if ['solor', 'app', 'app_master'].include?(node[:instance_role])
       source "dj"
     end
     
-    template "/etc/monit.d/delayed_job_worker.#{app_name}.monitrc" do
-      source "delayed_job.monit.erb"
-      owner "root"
-      group "root"
-      mode 0644
-      variables({
-        :app_name => app_name,
-        :user => node[:owner_name],
-        :worker_name => worker_name,
-        :framework_env => node[:environment][:framework_env]
-      })
+    worker_count = 1
+    
+    case node[:ec2][:instance_type]
+    when 'm1.small': worker_count = 2
+    when 'c1.medium': worker_count = 4
+    when 'c1.xlarge': worker_count = 8
+    else
+      worker_count = 2
+    end
+    
+    worker_count.times do |count|
+      template "/etc/monit.d/delayed_job_worker.#{count+1}.#{app_name}.monitrc" do
+        source "delayed_job.monit.erb"
+        owner "root"
+        group "root"
+        mode 0644
+        variables({
+          :app_name => app_name,
+          :user => node[:owner_name],
+          :worker_name => "#{app_name}_deplayed_job_#{count+1}",
+          :framework_env => node[:environment][:framework_env]
+        })
+      end
     end
     
     bash "monit-reload-restart" do
