fbe85fdd1a64e10999d402ff9b2fb8f0b1d1d6b3
podman <podman@gmail.com>
2013-08-08 17:06:04 -0400

    proper file permissions

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -3,13 +3,13 @@ if ['app', 'app_master'].include?(node[:instance_role])
 		source "server.conf.erb"
 		owner node[:owner_name]
 		group node[:owner_name]
-		mode 0755
+		mode 0644
 	end
 
 	template "/etc/nginx/servers/keep.sproutvideo.ssl.conf" do
 		source "server.ssl.conf.erb"
 		owner node[:owner_name]
 		group node[:owner_name]
-		mode 0755
+		mode 0644
 	end
 end
