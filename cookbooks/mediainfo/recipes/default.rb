eeb941fc2451273b3533d9d52b304119e07e3002
podman <podman@gmail.com>
2013-08-08 15:47:40 -0400

    use gentoo package instead of installing from source

--- cookbooks/mediainfo/recipes/default.rb
+++ cookbooks/mediainfo/recipes/default.rb
@@ -3,28 +3,14 @@ node[:applications].each do |app_name,data|
 
   case node[:instance_role]
     when "solo", "app", "app_master", "util"
-      mi_dir = "MediaInfo_CLI_GNU_FromSource"
-      mi_file = "MediaInfo_CLI_0.7.63_GNU_FromSource.tar.bz2"
-      mi_url = "http://downloads.sourceforge.net/project/mediainfo/binary/mediainfo/0.7.63/MediaInfo_CLI_0.7.63_GNU_FromSource.tar.bz2"
-      
-      remote_file "/data/#{mi_file}" do
-        source "#{mi_url}"
-        owner node[:owner_name]
-        group node[:owner_name]
-        mode 0644
-        backup 0
-        not_if { FileTest.exists?("/data/#{mi_file}") }
+      enable_package "media-video/mediainfo" do
+        version "0.7.64"
       end
 
-      execute "unarchive mi-to-install" do
-        command "cd /data && tar jxf #{mi_file} && sync"
-        #not_if { FileTest.directory?("/data/#{mi_dir}") } always extract because can't tell from the dir name if it's new or not
+      package "dev-db/redis" do
+        version "0.7.64"
+        action :upgrade
       end
-      
-      execute "build mi package" do
-        command "cd /data/#{mi_dir} && ./CLI_Compile.sh && cd /data/#{mi_dir}/MediaInfo/Project/GNU/CLI && make install"
-        not_if { FileTest.exists?("/usr/local/bin/mediainfo") && !`mediainfo --Version`.match('0.7.63').nil? }
-      end 
       
     end
   end
